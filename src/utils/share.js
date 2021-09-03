b2501c9164a2c8b9086d3c1ccf93823b607fdc20
朱双龙 <eddie@Eddie.local>
2021-09-03 16:12:44 +0800

    参数调整

--- src/utils/share.js
+++ src/utils/share.js
@@ -5,14 +5,12 @@ export function share({ that, title, content, shareImage}) {
     const { code, data } = res
     if (code === 20000 && data) {
       const { config } = data
-      alert(config)
-      alert(133)
-      // alert(config.appid)
+      alert(config.appid)
       wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: config.appid, // 必填，公众号的唯一标识
+        appId: config.appId, // 必填，公众号的唯一标识
         timestamp: config.timestamp, // 必填，生成签名的时间戳
-        nonceStr: config.noncestr, // 必填，生成签名的随机串
+        nonceStr: config.nonceStr, // 必填，生成签名的随机串
         signature: config.signature, // 必填，签名，见附录1
         jsApiList: [
           "onMenuShareTimeline",
