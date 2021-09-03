0f26b500f379705a2ab6e0506cbc6b99f37f68e9
朱双龙 <eddie@Eddie.local>
2021-09-03 16:21:26 +0800

    参数调整

--- src/utils/share.js
+++ src/utils/share.js
@@ -5,7 +5,7 @@ export function share({ that, title, content, shareImage}) {
     const { code, data } = res
     if (code === 20000 && data) {
       const { config } = data
-      alert(config.appid)
+      alert(title, content, shareImage)
       wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: config.appId, // 必填，公众号的唯一标识
@@ -16,18 +16,21 @@ export function share({ that, title, content, shareImage}) {
           "onMenuShareTimeline",
           "onMenuShareAppMessage",
         ], // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-      });
+      })
+
       wx.ready(function () {
+        alert(123456)
         shareFunction()
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发>时才调用的接口，则可以直接调用，不需要放在ready函数中。
-      });
+      })
     }
   }, err => {
+    alert(err)
     this.$Toast(err)
   })
 
   function shareFunction() {
-    //分享到朋友圈
+    // 分享到朋友圈
     wx.onMenuShareTimeline({
       title: title, // 分享标题
       desc: content, // 分享描述
@@ -40,7 +43,7 @@ export function share({ that, title, content, shareImage}) {
         // 用户取消分享后执行的回调函数
       },
     });
-    //分享给朋友
+    // 分享给朋友
     wx.onMenuShareAppMessage({
       title: title, // 分享标题
       desc: content, // 分享描述
