b7605a61fb1bef08155551286239e0b64f9b2b6a
朱双龙 <eddie@Eddie.lan>
2021-09-04 13:32:55 +0800

    分享测试

--- src/utils/share.js
+++ src/utils/share.js
@@ -20,7 +20,8 @@ export function share({ that, title, content, shareImage}) {
         console.log('ready')
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发>时才调用的接口，则可以直接调用，不需要放在ready函数中。
         // 分享到朋友圈
-        wx.onMenuShareAppMessage({
+        wx.updateAppMessageShareData({
+        // wx.onMenuShareAppMessage({
           title: title, // 分享标题
           desc: content, // 分享描述
           link: location.href, // 分享链接
@@ -28,11 +29,12 @@ export function share({ that, title, content, shareImage}) {
           success: function () {},
           cancel: function () {},
           complete: function(res) {
-            console.log('onMenuShareAppMessage complete')
+            console.log('updateAppMessageShareData complete')
           }
         })
         // 分享给朋友
-        wx.onMenuShareTimeline({
+        wx.updateTimelineShareData({
+        // wx.onMenuShareTimeline({
           title: title, // 分享标题
           desc: content, // 分享描述
           link: location.href, // 分享链接
