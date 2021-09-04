f276cf8582cbd702e3fcfc8dcf5eac533ba0bc4b
wufly <wfxykzd@163.com>
2021-09-04 12:39:57 +0800

    update

--- src/utils/share.js
+++ src/utils/share.js
@@ -20,6 +20,7 @@ export function share({ that, title, content, shareImage}) {
       })
 
       wx.ready(function () {
+        console.log(1233)
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发>时才调用的接口，则可以直接调用，不需要放在ready函数中。
         // 分享到朋友圈
         wx.onMenuShareTimeline({
@@ -40,10 +41,6 @@ export function share({ that, title, content, shareImage}) {
           cancel: function () {},
         })
       })
-      wx.error(function(res){
-        console.log("error",res)
-        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-      });
     } else {
       alert(`系统异常，code非200${msg}`)
     }
