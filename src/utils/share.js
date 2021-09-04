8282ddb124a5a00d9e68bc6ae26ca6a9b9aabfa8
朱双龙 <eddie@Eddie.lan>
2021-09-04 12:00:05 +0800

    测试分享

--- src/utils/share.js
+++ src/utils/share.js
@@ -3,10 +3,10 @@ export function share({ that, title, content, shareImage}) {
     url: location.href
   }).then(res => {
     console.log(res)
-    // const { code, data, msg } = res
+    const { code, data, msg } = res
     
-    if (res.code === 20000) {
-      var config = res.data.config
+    if (code === 20000) {
+      const { config } = data
       wx.config({
         // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来
         appId: config.appId, // 必填，公众号的唯一标识
@@ -14,18 +14,15 @@ export function share({ that, title, content, shareImage}) {
         nonceStr: config.nonceStr, // 必填，生成签名的随机串
         signature: config.signature, // 必填，签名，见附录1
         jsApiList: [
-          "updateTimelineShareData",
-          "updateAppMessageShareData",
-          // "onMenuShareTimeline",
-          // "onMenuShareAppMessage",
+          "onMenuShareTimeline",
+          "onMenuShareAppMessage",
         ], // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       })
 
       wx.ready(function () {
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发>时才调用的接口，则可以直接调用，不需要放在ready函数中。
         // 分享到朋友圈
-        wx.updateTimelineShareData({ 
-        // wx.onMenuShareTimeline({
+        wx.onMenuShareTimeline({
           title: title, // 分享标题
           desc: content, // 分享描述
           link: location.href, // 分享链接
@@ -34,8 +31,7 @@ export function share({ that, title, content, shareImage}) {
           cancel: function () {},
         })
         // 分享给朋友
-        wx.updateAppMessageShareData({
-        // wx.onMenuShareAppMessage({
+        wx.onMenuShareAppMessage({
           title: title, // 分享标题
           desc: content, // 分享描述
           link: location.href, // 分享链接
@@ -45,12 +41,12 @@ export function share({ that, title, content, shareImage}) {
         })
       })
     } else {
-      alert(res.msg || '系统异常')
+      alert(`系统异常，code非200${msg}`)
     }
   })
 
   wx.error(function (res) {
-    alert(res.errMsg)
+    alert(`系统异常error${res.errMsg}`)
   })
 }
 
