5712638e6d9289a957d928c2063fef044158f4fe
wufly <wfxykzd@163.com>
2021-09-04 13:03:27 +0800

    update

--- src/utils/share.js
+++ src/utils/share.js
@@ -20,7 +20,7 @@ export function share({ that, title, content, shareImage}) {
         console.log(1233)
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发>时才调用的接口，则可以直接调用，不需要放在ready函数中。
         // 分享到朋友圈
-        wx.updateAppMessageShareData({
+        wx.onMenuShareAppMessage({
           title: title, // 分享标题
           desc: content, // 分享描述
           link: location.href, // 分享链接
@@ -29,7 +29,7 @@ export function share({ that, title, content, shareImage}) {
           cancel: function () {},
         })
         // 分享给朋友
-        wx.updateTimelineShareData({
+        wx.onMenuShareTimeline({
           title: title, // 分享标题
           desc: content, // 分享描述
           link: location.href, // 分享链接
