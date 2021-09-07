5f8a4e6e4082fad3b0b4bf95b1480af2e9946486
wufly <wfxykzd@163.com>
2021-09-07 10:42:02 +0800

    test

--- src/utils/share.js
+++ src/utils/share.js
@@ -17,6 +17,14 @@ export function share({ that, title, content, shareImage}) {
       })
 
       wx.ready(function () {
+        wx.checkJsApi({
+          jsApiList: ['updateAppMessageShareData'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+          success: function(res) {
+              // 以键值对的形式返回，可用的api值true，不可用为false
+              // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+              console.log("check res:",res)
+          }
+      });
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发>时才调用的接口，则可以直接调用，不需要放在ready函数中。
         // 分享到朋友圈
         wx.updateAppMessageShareData({
