e67c7b551a1a8633654240ee7a75b2c9bbff58f4
wufly <wfxykzd@163.com>
2021-10-15 14:34:22 +0800

    update

--- src/pages/category/Index.vue
+++ src/pages/category/Index.vue
@@ -87,7 +87,6 @@ export default {
   },
   methods: {
     getTpyes() {
-      const { id } = route.params
       this.request('ProductTypes', {styleId: this.$route.params.id}).then((res) => {
         const { code, data } = res
         if (code === 20000 && data) {
